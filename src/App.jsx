43e163a6e02ad4131cb024b1f9abeff85ed091a8
vapppu <veera.hiltunen@gmail.com>
2023-09-27 12:22:27 +0300

    Add button that decreases counter value

--- src/App.jsx
+++ src/App.jsx
@@ -15,14 +15,17 @@ const Button = (props) => {
 const App = () => {
   const [ counter, setCounter ] = useState(0)
 
-  const increaseByOne = () => setCounter(counter + 1)
-  const setToZero = () => setCounter(0)
+  const increaseByOne = () => {console.log("Increasing from value", counter); setCounter(counter + 1)}
+  const decreaseByOne = () => {console.log("Decreasing from value", counter); setCounter(counter - 1)}
+  const setToZero = () => {console.log("Setting to zero from value", counter); setCounter(0)}
 
   return (
     <>
       <Display counter = {counter}/>
       <Button handleClick={increaseByOne} text='plus'/>
       <Button handleClick={setToZero} text='zero'/>
+      <Button handleClick={decreaseByOne} text='minus'/>
+
     </>
   )
 }
