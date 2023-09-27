607b5e254bfba0c4fce0fb2fa88c8bfe097def9d
vapppu <veera.hiltunen@gmail.com>
2023-09-27 11:52:59 +0300

    Add button to set counter to zero

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,15 @@ const App = () => {
   const [ counter, setCounter ] = useState(0)
 
   const handleClick = () => {
-    console.log('clicked')
+    setCounter(counter +1)
+    console.log(counter)
   }
 
   return (
     <>
       <div>{counter}</div>
       <button onClick={handleClick}>plus</button>
+      <button onClick={() => setCounter(0)}>zero</button>
     </>
   )
 }
