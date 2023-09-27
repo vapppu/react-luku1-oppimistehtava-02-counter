7fe70a58a06b9ca381843e67d843f9392f81e2b2
vapppu <veera.hiltunen@gmail.com>
2023-09-27 12:27:04 +0300

    Format document

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,35 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-const Display = ({counter}) => <div>{counter}</div>
+const Display = ({ counter }) => <div>{counter}</div>;
 
-const Button = ({ handleClick, text}) => <button onClick={handleClick}>{text}</button>
-
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
 
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
+  const [counter, setCounter] = useState(0);
 
-  const increaseByOne = () => {console.log("Increasing from value", counter); setCounter(counter + 1)}
-  const decreaseByOne = () => {console.log("Decreasing from value", counter); setCounter(counter - 1)}
-  const setToZero = () => {console.log("Setting to zero from value", counter); setCounter(0)}
+  const increaseByOne = () => {
+    console.log("Increasing from value", counter);
+    setCounter(counter + 1);
+  };
+  const decreaseByOne = () => {
+    console.log("Decreasing from value", counter);
+    setCounter(counter - 1);
+  };
+  const setToZero = () => {
+    console.log("Setting to zero from value", counter);
+    setCounter(0);
+  };
 
   return (
     <>
-      <Display counter = {counter}/>
-      <Button handleClick={increaseByOne} text='plus'/>
-      <Button handleClick={setToZero} text='zero'/>
-      <Button handleClick={decreaseByOne} text='minus'/>
-
+      <Display counter={counter} />
+      <Button handleClick={increaseByOne} text="plus" />
+      <Button handleClick={setToZero} text="zero" />
+      <Button handleClick={decreaseByOne} text="minus" />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
