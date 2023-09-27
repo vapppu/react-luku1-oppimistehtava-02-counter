5dcb730d4ccebb86617b4794e766c8f19fd0a232
vapppu <veera.hiltunen@gmail.com>
2023-09-27 12:25:36 +0300

    Simplify Display component with inline arrow syntax

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
 import { useState } from 'react'
 
-const Display = ({counter}) => {
-  return (
-    <div>{counter}</div>
-  )
-}
+const Display = ({counter}) => <div>{counter}</div>
 
 const Button = ({ handleClick, text}) => <button onClick={handleClick}>{text}</button>
 
